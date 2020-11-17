965dcba2bc812b8d084cbc4c1ebc9ac67fff3e57
naoki132 <naoki5.ab@gmail.com>
2020-11-17 16:05:11 +0900

    appdate

--- napsak.py
+++ napsak.py
@@ -9,39 +9,46 @@ with open('p01_w.txt', mode='r') as f:
 with open('p01_p.txt', mode='r') as f:
     p = [s.strip() for s in f.readlines()]
 
+#ナップサックに入る容量
 c_limit = int(c)
+#荷物の数
 item = len(w)
-
+#個体数
 x = 9
 
 e = [[0] * item for _ in range(x)]
-
+#初期値を乱数で生成、代入
 for i in range(x):
     for j in range(item):
         e[i][j] = r.randint(0,1)
 
 w_sum = [0] * x
 p_sum = [0] * x
+pout_list = [0] * x
 
 top2 = [0] * 2
 n_top2 = [0] * 2
 
-elite = [[0] * item for i in range(2)]
-nexte = [[0] * item for i in range(x)]
+elite = [[0] * item for _ in range(2)]
+nexte = [[0] * item for _ in range(x)]
 
+#評価関数
 def evaluation():
     for i in range(x):
         w_sum[i] = 0
         p_sum[i] = 0
+        pout_list[i] = 0
 
         for j in range(item):
             if(e[i][j] == 1):
                 w_sum[i] += int(w[j])
                 p_sum[i] += int(p[j])
         
-            if(w_sum[i] > c_limit):
-                p_sum[i] = 0
+        if(w_sum[i] > c_limit):
+            p_sum[i] = 0
+            pout_list[i] += 1
 
+#選択関数
 def choice():
     if(p_sum[0] > p_sum[1]):
         top2[0] = p_sum[0]
@@ -63,12 +70,16 @@ def choice():
         elif(p_sum[i] > top2[1]):
             top2[1] = p_sum[i]
             n_top2[1] = i
-
+    #上位２つをeliteに代入
     for i in range(2):
         for j in range(item):
             elite[i][j] = e[n_top2[i]][j]
-
+    #ルーレット選択
     total = 0
+    for i in range(x):
+        if(pout_list[i] != 0):
+            p_sum[i] = 50
+    
     for i in range(x):
         total += p_sum[i]
 
@@ -82,16 +93,16 @@ def choice():
                     nexte[i][k] = e[j][k]
                 break
 
+#2点交叉
 def generation():
     for i in range(0,x,2):
-        if((x%2 == 1 and i < x-1) or (x%2 == 0 and i < x)):
+        if(x%2 == 1 and i >= x-1):
             break
         crossrate = r.randint(0,99)
         if(crossrate < 95):
             r1 = r.randint(0,item-1)
             r2 = r1 + r.randint(0,item-r1-1)
-            child = [[0] * item for i in range(2)]
-
+            child = [[0] * item for _ in range(2)]
             for j in range(item):
                 if(r1 <= j and j <= r2):
                     child[0][j] = nexte[i+1][j]
@@ -108,10 +119,10 @@ def mutation():
     #突然変異
     for i in range(x):
         mutantrate = r.randint(0,99)
-        if(mutantrate < 3):
+        if(mutantrate < 5):
             m = r.randint(0,item-1)
             nexte[i][m] = (nexte[i][m] + 1) % 2
-
+#エリート代入
 def inherit():
     for i in range(x):
         for j in range(item):
@@ -121,7 +132,8 @@ def inherit():
                 e[i][j] = nexte[i][j]
 
 if __name__ == "__main__":
-    for i in range(10000):
+    for i in range(1000):
+        print(p_sum)
         evaluation()
         choice()
         generation()
