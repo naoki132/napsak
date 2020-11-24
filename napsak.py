9113df7e1ba9947e0de7b931a9ac0224795b55c3
naoki132 <naoki5.ab@gmail.com>
2020-11-24 12:52:46 +0900

    ルーレット選択をランキング選択に変更

--- napsak.py
+++ napsak.py
@@ -24,20 +24,21 @@ for i in range(x):
 
 w_sum = [0] * x
 p_sum = [0] * x
-pout_list = [0] * x
+p_sum2 = [0] * x
+p_sum_index = [0] * x
+rank = [0] * x
 
 top2 = [0] * 2
 n_top2 = [0] * 2
 
 elite = [[0] * item for _ in range(2)]
-nexte = [[0] * item for _ in range(x)]
+nexte = [[0] * item for _ in range(x-2)]
 
 #評価関数
 def evaluation():
     for i in range(x):
         w_sum[i] = 0
         p_sum[i] = 0
-        pout_list[i] = 0
 
         for j in range(item):
             if(e[i][j] == 1):
@@ -46,10 +47,10 @@ def evaluation():
         
         if(w_sum[i] > c_limit):
             p_sum[i] = 0
-            pout_list[i] += 1
 
 #選択関数
 def choice():
+    #エリート選択
     if(p_sum[0] > p_sum[1]):
         top2[0] = p_sum[0]
         top2[1] = p_sum[1]
@@ -74,29 +75,36 @@ def choice():
     for i in range(2):
         for j in range(item):
             elite[i][j] = e[n_top2[i]][j]
-    #ルーレット選択
+    #ランキング選択
+    p_sum2 = p_sum.copy()
+    for i in range(x):
+        max = 0
+        max_index = 0
+        for j in range(x):
+            if(max <= p_sum2[j]):
+                max = p_sum2[j]
+                max_index = j
+        rank[max_index] = x - i
+        p_sum2[max_index] = -1
+
     total = 0
     for i in range(x):
-        if(pout_list[i] != 0):
-            p_sum[i] = 50
-    
-    for i in range(x):
-        total += p_sum[i]
+        total += rank[i]
 
-    for i in range(x):
+    for i in range(x-2):
         arrow = r.randint(0,total)
-        sum = 0
+        gokei = 0
         for j in range(x):
-            sum += p_sum[j]
-            if(sum >= arrow):
+            gokei += rank[j]
+            if(gokei >= arrow):
                 for k in range(item):
                     nexte[i][k] = e[j][k]
                 break
 
 #2点交叉
 def generation():
-    for i in range(0,x,2):
-        if(x%2 == 1 and i >= x-1):
+    for i in range(0,x-2,2):
+        if(x%2 == 1 and i >= x-3):
             break
         crossrate = r.randint(0,99)
         if(crossrate < 95):
@@ -117,7 +125,7 @@ def generation():
 
 def mutation():
     #突然変異
-    for i in range(x):
+    for i in range(x-2):
         mutantrate = r.randint(0,99)
         if(mutantrate < 5):
             m = r.randint(0,item-1)
@@ -129,10 +137,10 @@ def inherit():
             if(i == 0 or i == 1):
                 e[i][j] = elite[i][j]
             else:
-                e[i][j] = nexte[i][j]
+                e[i][j] = nexte[i-2][j]
 
 if __name__ == "__main__":
-    for i in range(1000):
+    for i in range(500):
         print(p_sum)
         evaluation()
         choice()
